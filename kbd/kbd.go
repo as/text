74a7a330b4ebb45df5744c74107f7ec617d78860
as <as.utf8@gmail.com>
2018-02-06 10:16:27 -0800

    text: mouse: use sendfirst in some places

--- kbd/kbd.go
+++ kbd/kbd.go
@@ -1,24 +1,10 @@
 package kbd
 
 import (
-	"github.com/as/frame/font"
 	"github.com/as/text"
 	"github.com/as/text/find"
 	"golang.org/x/mobile/event/key"
 )
-
-func setFont(ed text.Editor, size int) {
-	type Framer interface {
-		Dy() int
-		SetFont(*font.Font)
-		TTF() []byte
-	}
-	switch fr := ed.(type) {
-	case Framer:
-		fsize := 5 * fr.Dy() / 6
-		fr.SetFont(font.NewTTF(fr.TTF(), fsize))
-	}
-}
 
 // markDirt calls Mark if the editor implements
 // the find.Dirt interface
@@ -43,12 +29,12 @@ func SendClient(hc text.Editor, e key.Event) {
 		}
 
 		if e.Modifiers == key.ModControl {
-			df := 2
-			if key.CodeHyphenMinus == e.Code {
-				df = -2
-			}
-			setFont(hc, df)
-			return
+//			df := 2
+//			if key.CodeHyphenMinus == e.Code {
+//				df = -2
+//			}
+//			setFont(hc, df)
+//			return
 		}
 	case key.CodeUpArrow, key.CodePageUp, key.CodeDownArrow, key.CodePageDown:
 		n := 1
